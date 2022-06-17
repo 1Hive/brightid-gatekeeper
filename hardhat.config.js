eff1f52c3102e9e258f6502b75651da7fb983a50
Peter Grassberger <petertheone@gmail.com>
2022-06-17 12:26:09 +0200

    Switch Gnosis Chain rpc, old one deprecated
    https://developers.gnosischain.com/for-developers/developer-resources/update-rpc-url

--- hardhat.config.js
+++ hardhat.config.js
@@ -28,7 +28,7 @@ module.exports = {
       url: "https://mainnet.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", //<---- YOUR INFURA ID! (or it won't work)
     },
     xdai: {
-      url: "https://rpc.xdaichain.com/",
+      url: "https://rpc.gnosischain.com/",
       gasPrice: 1000000000,
       accounts: ["0xf3964c8e752d6ac832c917a019e8a1a6f70751c8f20e7ccc2149cc5621919d78"]
     },
